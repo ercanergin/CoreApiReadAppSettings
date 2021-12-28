eb5943d12ad1ab761b2f141f9e3e1689c888842b
ErcanErgin1 <96108622+ErcanErgin1@users.noreply.github.com>
2021-12-28 22:30:58 +0300

    Add project files.

---
+++ CoreApiReadAppSettings/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using CoreApiReadAppSettings.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiReadAppSettings.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Student")]
+    public class StudentController : Controller
+    {
+        private readonly IOptions<MySettingsModel> appSettings;
+
+        public StudentController(IOptions<MySettingsModel> app)
+        {
+            appSettings = app;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var dbValue = appSettings.Value.DbConnection;
+
+            var list = new List<string>();
+            list.Add("Ercan");
+            list.Add("Ergin");
+            return Ok(list);
+        }
+    }
+}
---
+++ CoreApiReadAppSettings/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiReadAppSettings.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        private IConfiguration configuration;
+        public UserController(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            //Method 1
+            string dbConn = configuration.GetSection("MySettings").GetSection("DbConnection").Value;
+
+            //Method 2
+            string dbConn2 = configuration.GetValue<string>("MySettings:DbConnection");
+
+            var list = new List<string>();
+            list.Add("Ercan");
+            list.Add("Ergin");
+            return Ok(list);
+        }
+    }
+}
